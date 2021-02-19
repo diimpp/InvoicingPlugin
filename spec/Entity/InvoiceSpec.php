359512bd6bb44c47589cc7e81e55cc4f9367f18d
Dmitri Perunov <dmitri.perunov@gmail.com>
2021-02-19 18:09:36 +0700

    Add spec for InvoiceSequence and InvoiceShopBillingData

--- spec/Entity/InvoiceSpec.php
+++ spec/Entity/InvoiceSpec.php
@@ -23,11 +23,11 @@ final class InvoiceSpec extends ObjectBehavior
         ChannelInterface $channel,
         InvoiceShopBillingDataInterface $shopBillingData
     ): void {
-        $issuedAt = \DateTimeImmutable::createFromFormat('Y-m', '2019-01');
+        $issuedAt = new \DateTimeImmutable('2019-01-23 15:45:30');
 
         $this->beConstructedWith(
             '7903c83a-4c5e-4bcf-81d8-9dc304c6a353',
-            $issuedAt->format('Y/m') . '/000000001',
+            '2019/01/000000001',
             '007',
             $issuedAt,
             $billingData,
@@ -51,23 +51,64 @@ final class InvoiceSpec extends ObjectBehavior
         $this->shouldImplement(ResourceInterface::class);
     }
 
-    function it_has_an_id(
-        BillingDataInterface $billingData,
-        LineItemInterface $lineItem,
-        TaxItemInterface $taxItem,
-        ChannelInterface $channel,
-        InvoiceShopBillingDataInterface $shopBillingData
-    ): void {
+    function it_has_id(): void
+    {
+        $this->getId()->shouldReturn('7903c83a-4c5e-4bcf-81d8-9dc304c6a353');
         $this->id()->shouldReturn('7903c83a-4c5e-4bcf-81d8-9dc304c6a353');
+    }
+
+    function it_has_a_number(): void
+    {
         $this->number()->shouldReturn('2019/01/000000001');
+    }
+
+    function it_has_an_order_number(): void
+    {
         $this->orderNumber()->shouldReturn('007');
-        $this->billingData()->shouldReturn($billingData);
+    }
+
+    function it_has_a_date_of_issuing(): void
+    {
+        $this->issuedAt()->shouldBeLike(new \DateTimeImmutable('2019-01-23 15:45:30'));
+    }
+
+    function it_has_a_billing_data(BillingDataInterface $billingData): void
+    {
+        $this->billingData()->shouldBeLike($billingData);
+    }
+
+    function it_has_a_currency_code(): void
+    {
         $this->currencyCode()->shouldReturn('USD');
+    }
+
+    function it_has_a_locale_code(): void
+    {
         $this->localeCode()->shouldReturn('en_US');
+    }
+
+    function it_has_total(): void
+    {
         $this->total()->shouldReturn(10300);
+    }
+
+    function it_has_line_items(LineItemInterface $lineItem): void
+    {
         $this->lineItems()->shouldBeLike(new ArrayCollection([$lineItem->getWrappedObject()]));
+    }
+
+    function it_has_tax_items(TaxItemInterface $taxItem): void
+    {
         $this->taxItems()->shouldBeLike(new ArrayCollection([$taxItem->getWrappedObject()]));
+    }
+
+    function it_has_a_channel(ChannelInterface $channel): void
+    {
         $this->channel()->shouldReturn($channel);
-        $this->shopBillingData()->shouldReturn($shopBillingData);
+    }
+
+    function it_has_a_shop_billing_data(InvoiceShopBillingDataInterface $shopBillingData): void
+    {
+        $this->shopBillingData()->shouldBeLike($shopBillingData);
     }
 }
--- src/Entity/InvoiceShopBillingData.php
+++ src/Entity/InvoiceShopBillingData.php
@@ -43,39 +43,14 @@ class InvoiceShopBillingData implements InvoiceShopBillingDataInterface, Resourc
         return $this->company;
     }
 
-    public function getTaxId(): ?string
-    {
-        return $this->taxId;
-    }
-
-    public function getCountryCode(): ?string
-    {
-        return $this->countryCode;
-    }
-
-    public function getStreet(): ?string
-    {
-        return $this->street;
-    }
-
-    public function getCity(): ?string
-    {
-        return $this->city;
-    }
-
-    public function getPostcode(): ?string
-    {
-        return $this->postcode;
-    }
-
-    public function getRepresentative(): ?string
-    {
-        return $this->representative;
-    }
-
     public function setCompany(?string $company): void
     {
         $this->company = $company;
+    }
+
+    public function getTaxId(): ?string
+    {
+        return $this->taxId;
     }
 
     public function setTaxId(?string $taxId): void
@@ -83,9 +58,19 @@ class InvoiceShopBillingData implements InvoiceShopBillingDataInterface, Resourc
         $this->taxId = $taxId;
     }
 
+    public function getCountryCode(): ?string
+    {
+        return $this->countryCode;
+    }
+
     public function setCountryCode(?string $countryCode): void
     {
         $this->countryCode = $countryCode;
+    }
+
+    public function getStreet(): ?string
+    {
+        return $this->street;
     }
 
     public function setStreet(?string $street): void
@@ -93,14 +78,29 @@ class InvoiceShopBillingData implements InvoiceShopBillingDataInterface, Resourc
         $this->street = $street;
     }
 
+    public function getCity(): ?string
+    {
+        return $this->city;
+    }
+
     public function setCity(?string $city): void
     {
         $this->city = $city;
     }
 
+    public function getPostcode(): ?string
+    {
+        return $this->postcode;
+    }
+
     public function setPostcode(?string $postcode): void
     {
         $this->postcode = $postcode;
+    }
+
+    public function getRepresentative(): ?string
+    {
+        return $this->representative;
     }
 
     public function setRepresentative(?string $representative): void
